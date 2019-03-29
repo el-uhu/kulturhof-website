2220490fd3eed1cb7b2062be0c4543e91e5e97ee
Lukas Hutter <luki@pop-os.localdomain>
2019-03-29 19:24:45 +0100

    trying to make typography look nice on phones, but not quite succeeding

--- themes/kulturhof-hugo/static/js/main.js
+++ themes/kulturhof-hugo/static/js/main.js
@@ -1,3 +1,4 @@
+// When the user scrolls down 250px from the top of the document, resize the header
 $('#content').scroll(function() {
     if ($(this).scrollTop() > 250){
         $('#header').addClass("smaller");
@@ -7,5 +8,5 @@ $('#content').scroll(function() {
     }
 });
 
-// When the user scrolls down 50px from the top of the document, resize the header's font size
+
 // window.onscroll = function() {test()};
